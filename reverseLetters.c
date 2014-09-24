ff1e8253926fda67b6cd0fc84f84b78610235d38
francismwanza <francismwanza@yahoo.fr>
2014-09-24 16:35:41 -0300

    updates

--- reverseLetters.c
+++ reverseLetters.c
@@ -1,12 +1,3 @@
-// Write a method/procedure (in any programming or declarative language) that takes in a string parameter composed of digits and letters only 
-//(you don't need to validate the string). The method should reverse all letter and keep the digits in their original position.
-// Ex.: ab1c2de345f => fe1d2cb345a
-
-//How to compile
-//gcc ahmed.c -o ahmed
-//How to execute
-// ./ahmed <InputString>
-
 
 #include <string.h>
 #include <stdlib.h>
